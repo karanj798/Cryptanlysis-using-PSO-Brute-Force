87d3a25937c64e9c35bf7ded54871d580a3ce4ce
Karan Jariwala <karanj798@gmail.com>
2018-11-24 19:38:09 -0500

    Update pso.py

--- pso.py
+++ pso.py
@@ -1,8 +1,8 @@
-import numpy as np
 import math
 import random
+
 '''
-Frequencies:
+Frequencies of English characters:
 A :  8.55        K :  0.81        U :  2.68
 B :  1.60        L :  4.21        V :  1.06
 C :  3.16        M :  2.53        W :  1.83
@@ -16,7 +16,6 @@ J :  0.22        T :  8.94
 '''
 class Particle:
 	
-	
 	def __init__ (self):
 		self.velocity = []
 		self.position = []
@@ -27,7 +26,7 @@ class Particle:
 		# initialize a random position and random velocity
 		for i in range(0, self.keyLen):                  # iterate through the length of key.
 			self.position.append(random.random()*26)     # randomly initializing particle's position
-			self.velocity.append(random.uniform(0, 1))   # ranomly initializing particle's	velocity 
+			self.velocity.append(random.uniform(0, 1))   # ranomly initializing particle's velocity 
 		
 		
 	def updateVelocity (self, gBestPos, gBestErr):
@@ -50,16 +49,23 @@ class Particle:
 		# calculate social velocity		
 		#resulting velocity = vector sum of all 3 of the above
 		
-	def updatePosition (self):
+	def updatePosition (self, bounds):
 		for i in range (0, keyLen):
 			self.position[i] = self.position[i] + self.velocity[i]
-			if 
+			
+			if self.position[i] > bounds[i][1]:
+				self.position[i] = bounds[i][1]
+				
+			if self.position[i] < bounds[i][0]:
+				self.position[i] = bounds[i][0]
+			
 			
 		#just add the velocity to the position in each dimension
 		
 	def calcError (self, fitnessFunc):
-		for i in range (0, keyLen):
-			
+		if self.currError < self.bestError or self.bestError == -1:
+			self.pBestSelf = self.position
+			self.bestError = self.currError
 		#implement this by yourself
 		# remember that this must update the currError thingy, and the bestError (for self)
 
@@ -70,11 +76,36 @@ class PSO:
 	swarm = []
 	errorBestGlobal = -1
 	
-	def __init__ (self, maxIter, numParticles):
+	def __init__ (self, maxIter, numParticles, bounds, fitnessFunc):
 		#create a particle using the constructor n times based on numParticles
+		global dimensions
+		dimensions = 2    # 2-D dimensions right???
+		bestErrorG = -1
+		bestPositionG = []
+		swarm = []
 		
-		
-	def runPSO():
+		for i in range(0, numParticles):
+			p = Particle()
+			swarm.append(p)
+		i = 0
+		while i < maxIter:
+			for j in range (0, numParticles):
+				swarm[j].calcError(fitnessFunc)
+				
+				if swarm[j].currError < bestErrorG or bestErrorG == -1:
+					pBestSelf = list(swarm[j].position)
+					bestErrorG = float(swarm[j].currError)
+			
+			for j in range(0, numParticles):
+				swarm[j].updateVelocity(bestPositionG)
+				swarm[j].updatePosition(bounds)
+			
+			i = i + 1
+			
+		print("Final")
+		print(bestPositionG)
+		print(bestErrorG)
+	'''def runPSO():
 		for i in range(0, len(maxIter)):
 			#calcError() for each particle
 			
@@ -85,7 +116,28 @@ class PSO:
 			#update position based on velocity
 			
 			#repeat
-			
-			
+	'''
+
+def fitnessFunc():
+#input: currentPosition
+    stdFreq = {'A': 8.55, 'B': 1.60, 'C': 3.16, 'D': 3.87, 'E':12.10, 'F': 2.18, 'G': 2.09, 'H': 4.96, 'I': 7.33, 'J': 0.22,
+                'K': 0.81, 'L': 4.21, 'M': 2.53, 'N': 7.17, 'O': 7.47, 'P': 2.07, 'Q': 0.10, 'U': 2.68, 'R': 6.33, 'S': 6.73,
+                'T': 8.94, 'V': 1.06, 'W': 1.83, 'X': 0.19, 'Y': 1.72, 'Z': 0.11}
+	
+    decryText = "KMMABSUKZCSSEIIDCLCXENZPIYXGCDLYDGOMUWDOYHZILEYDMLGWMCPYAYFIQABITOVWZSNELEPDOHYIENBKVCPSSNNMLKPMMCXE"
+    obFreq = {}
+    for w in decryText:
+        obFreq[w] = decryText.count(w)
+    
+    fitness = []
+    diff = 0
+
+    for w in stdFreq:
+        if w in obFreq:
+            diff = abs(diff + stdFreq[w] - (obFreq[w]/len(decryText)))
+    print(diff)
+
+fitnessFunc()
+
 
 	
